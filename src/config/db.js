742f205ea1bdf523fd16856a3a5971a4c32341f5
zayimhan <zayimhankorkmaz@gmail.com>
2026-04-05 17:06:51 +0300

    add DB_PORT for railway mysql

--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,7 @@ export const getPool = () => {
   if (!pool) {
     pool = mysql.createPool({
       host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
